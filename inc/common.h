07a539981ec06ea42bbc09503a14108646079a66
hekang <hekangbit@163.com>
2023-08-13 22:24:46 +0800

    init cmake env

---
+++ inc/common.h
@@ -0,0 +1,10 @@
+#ifndef LOX_COMMON_H
+#define LOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "common.h"
+
+using namespace std;
+
+int main(int argc, char **args) {
+    cout << "hello" << endl;
+    return 0;
+}
