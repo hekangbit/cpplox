01da03898b4c49c6e9197a234629576cabc4d90b
hekang <hekangbit@163.com>
2025-09-03 10:25:20 +0800

    call init method as ctor

--- src/loxclass.cpp
+++ src/loxclass.cpp
@@ -5,6 +5,10 @@
 LoxClass::LoxClass(string name, map<string, lox_func_t> methods) : name(name), methods(methods) {}
 
 int LoxClass::Arity() const {
+  auto it = methods.find("init");
+  if (it != methods.end()) {
+    return it->second->Arity();
+  }
   return 0;
 }
 string LoxClass::toString() const {
@@ -13,5 +17,10 @@ string LoxClass::toString() const {
 
 Value LoxClass::Call(Interpreter &interpreter, vector<Value> &arguments) {
   auto instance = make_shared<LoxInstance>(this);
+  auto it = methods.find("init");
+  if (it != methods.end()) {
+    auto ctor = it->second->Bind(instance);
+    ctor->Call(interpreter, arguments); // run construct
+  }
   return Value(instance);
 }
